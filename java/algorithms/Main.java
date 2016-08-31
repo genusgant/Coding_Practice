091af5d975d640774856853113578dc61be83a7f
genus antony <genusgant@gmail.com>
2016-08-31 11:43:38 -0700

    Transfer Repo

---
+++ java/algorithms/Main.java
@@ -0,0 +1,38 @@
+package algorithms;
+
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+
+	}
+
+}
+
+
+
+/*    WordCounter
+
+		String input ="I had build a search engine application which will work in both desktop and internet crawling for data. And I am familiar with numpy, pandas, matplotlib and Map Reduce concept for data analysis.";
+				
+		WordCounter w1 = new WordCounter(input);
+		System.out.println(w1.print());
+		System.out.println(w1.sort(true));
+		System.out.println(w1.sort(false));
+		System.out.println(w1.Top());
+		System.out.println(w1.bottom());
+
+		for(Entry<String, Integer> e : w1.print().entrySet())
+		{
+			System.out.println("Key : "+e.getKey()+"; Value : "+e.getValue());
+		}
+		
+
+
+*/
